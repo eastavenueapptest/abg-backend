e347afde099abea0f9c84cd837dcbc87cd36c3c0
dwincruz <aldwinceazarcruz@gmail.com>
2025-06-29 01:38:05 +0800

    test cookies3

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
     store: sessionStore,
     cookie: {
       secure: false,
-      sameSite: "lax",
+      // sameSite: "lax",
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
