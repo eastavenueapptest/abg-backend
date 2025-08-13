bd5673faa48cae2955c53837f24c99b2eab85c37
dwincruz <aldwinceazarcruz@gmail.com>
2025-08-14 02:47:28 +0800

    new updates to email

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
   })
 );
