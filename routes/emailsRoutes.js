55f901e83ae75b93a3d09a050691aa6c0c328f7e
dwincruz <aldwinceazarcruz@gmail.com>
2025-09-09 03:07:25 +0800

    added changes

--- routes/emailsRoutes.js
+++ routes/emailsRoutes.js
@@ -4,8 +4,9 @@ const emailController = require("../controllers/emailController");
 
 router.post("/send-abg-form", emailController.sendAbgFormEmail);
 
-router
-  .route("/generate-secrey-key/:username")
-  .get(emailController.handleSendGeneratekey);
+router.get(
+  "/generate-secret-key/:username",
+  emailController.handleSendGeneratekey
+);
 
 module.exports = router;
