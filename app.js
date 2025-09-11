343bafc144bb0fc18ad6b8f2f325719642fa5e61
dwincruz <aldwinceazarcruz@gmail.com>
2025-09-11 14:40:57 +0800

    modify allowed headers

--- app.js
+++ app.js
@@ -41,7 +41,13 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
   })
 );
 
