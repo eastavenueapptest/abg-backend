29168d355dce0b1d2f93ff715e2adbde312605a1
dwincruz <aldwinceazarcruz@gmail.com>
2025-09-13 04:42:51 +0800

    change port

--- utils/emailUtils.js
+++ utils/emailUtils.js
@@ -4,7 +4,9 @@ const path = require("path");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.NODE_APP_GOOGLE_EMAIL,
     pass: process.env.NODE_APP_GOOGLE_PASS,
